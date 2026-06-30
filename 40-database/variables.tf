d88b18bec3e2f941d591046e3b68b09502bb02b3
sowmyataraka <sowmya.gundaji@gmail.com>
2026-06-30 17:18:12 +0100

    oil and karam added

--- 40-database/variables.tf
+++ 40-database/variables.tf
@@ -7,11 +7,11 @@ variable "environment" {
 }
 
 variable "zone_id" {
-    default = "Z07086101C1CVP7AT2UK4"
+    default = "Z0184746WRRFG0I0XJTP"
 }  
 
 variable "domain_name" {
-    default = "daws90s.shop"
+    default = "daws90s.uk"
 }
 
 variable "mysql_root_password" {

426c79d7809ae030340b5571262a682bf1e10856
sowmyataraka <sowmya.gundaji@gmail.com>
2026-07-03 07:26:22 +0100

    oil and karam added

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -9,7 +9,8 @@ chmod -R 755 /var/log/roboshop
 touch /var/log/roboshop/ansible.log
 
 cd /home/ec2-user
-git clone https://github.com/daws-90s/roboshop-ansible-v3.git
+git clone https://github.com/sowmyataraka/roboshop-ansible-v3.git
+
 cd roboshop-ansible-v3
 git pull
 ansible-playbook -e component=$component -e env=$environment roboshop.yaml

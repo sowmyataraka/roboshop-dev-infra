568a738d5cb71998048cf53554900a2e739d70af
sowmyataraka <sowmya.gundaji@gmail.com>
2026-07-05 12:28:42 +0100

    oil and karam added

---
+++ 80-frontend-alb/variable.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "daws90s.uk"
+}
+
+variable "zone_id" {
+    default = "Z0184746WRRFG0I0XJTP"
+}  

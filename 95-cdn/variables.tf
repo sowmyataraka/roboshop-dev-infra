6465c223257bdda92930482764a81b9740e13513
sowmyataraka <sowmya.gundaji@gmail.com>
2026-07-06 11:41:03 +0100

    Update files

---
+++ 95-cdn/variables.tf
@@ -0,0 +1,15 @@
+variable "environment" {
+    default = "dev"
+}
+
+variable "project" {
+    default = "roboshop"
+}
+
+variable "zone_id" {
+    default = "Z0184746WRRFG0I0XJTP"
+}  
+
+variable "domain_name" {
+    default = "daws90s.uk"
+}9050

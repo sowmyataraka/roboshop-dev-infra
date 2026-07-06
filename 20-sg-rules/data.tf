d1413f32f6cb3909e0ce22744b2d7f6584e47330
sowmyataraka <sowmya.gundaji@gmail.com>
2026-07-06 14:32:23 +0100

    update files

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -50,8 +50,8 @@ data "aws_ssm_parameter" "bastion_sg_id" {
     name = "/${var.project}/${var.environment}/bastion_sg_id"
 }
 
-data "http" "my_public_ip" {
-  url = "https://ipv4.icanhazip.com"
+data "aws_ssm_parameter" "vpn_sg_id" {
+    name = "/${var.project}/${var.environment}/vpn_sg_id"
 }
 
 data "http" "my_public_ip" {

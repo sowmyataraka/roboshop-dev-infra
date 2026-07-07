aacc1a44ee4637d5bd48e151e22daa139deaf7ac
sowmyataraka <sowmya.gundaji@gmail.com>
2026-07-07 05:38:12 +0100

    update files

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -5,6 +5,7 @@ resource "aws_route53_record" "mongodb" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
+ allow_overwrite = true
 }
 
 resource "aws_route53_record" "redis" {
@@ -13,6 +14,7 @@ resource "aws_route53_record" "redis" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "rabbitmq" {
@@ -21,6 +23,7 @@ resource "aws_route53_record" "rabbitmq" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "mysql" {
@@ -29,4 +32,5 @@ resource "aws_route53_record" "mysql" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
 }
